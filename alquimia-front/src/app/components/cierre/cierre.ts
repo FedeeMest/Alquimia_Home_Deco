c9584b5d4c8b60f9b778c2bf9c1994fefd8a2074
Fede Mestre <fede.mestre01@gmail.com>
2026-01-09 18:51:54 -0300

    change_route_cierre_caja

--- alquimia-front/src/app/components/cierre/cierre.ts
+++ alquimia-front/src/app/components/cierre/cierre.ts
@@ -89,7 +89,7 @@ export class CierreCajaComponent implements OnInit {
     this.cierreService.cerrar(payload).subscribe({
       next: () => {
         this.notificationService.show('Caja cerrada correctamente', 'success');
-        this.router.navigate(['/dashboard']); // O a donde prefieras
+        this.router.navigate(['/historial-cierres']); // O a donde prefieras
       },
       error: (err) => {
         console.error(err);

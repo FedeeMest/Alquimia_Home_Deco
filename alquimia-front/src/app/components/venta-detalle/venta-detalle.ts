fb7cce56ea20912db9dd7daf149fab9a105aa2eb
Fede <Fede.mestre01@gmail.com>
2026-02-14 11:04:22 -0300

    Revert "add_print_pdf"
    This reverts commit 54ecc97a4e11e54caf9733c46ca21d6b139755d6.

--- alquimia-front/src/app/components/venta-detalle/venta-detalle.ts
+++ alquimia-front/src/app/components/venta-detalle/venta-detalle.ts
@@ -5,8 +5,6 @@ import { VentaService } from '../../services/venta.service';
 import { Venta } from '../../Interfaces/venta.interface';
 import { FormsModule } from '@angular/forms';
 import { NotificationService } from '../../services/notification.service';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-venta-detalle',
@@ -22,7 +20,6 @@ export class VentaDetalleComponent implements OnInit {
 
   venta?: Venta;
   loading = true;
-  generandoPDF = false;
 
   editandoNotas = false;
   notasCache = '';
@@ -61,47 +58,6 @@ export class VentaDetalleComponent implements OnInit {
       this.notasCache = this.venta.observaciones || '';
     }
   }
-
-  descargarPDF() {
-    // 1. Verificamos que haya venta cargada
-    if (!this.venta) return;
-
-    this.generandoPDF = true;
-    
-    // 2. Buscamos el HTML que queremos "fotografiar"
-    // Asegúrate de que en tu HTML el div del ticket tenga id="comprobante-contenido"
-    const data = document.getElementById('comprobante-contenido'); 
-
-    if (data) {
-      html2canvas(data, { scale: 2 }).then(canvas => {
-        // Configuraciones A4
-        const imgWidth = 208; 
-        const imgHeight = canvas.height * imgWidth / canvas.width;
-        
-        const contentDataURL = canvas.toDataURL('image/png');
-        const pdf = new jsPDF('p', 'mm', 'a4');
-        const position = 0;
-        
-        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-        
-        pdf.save(`Comprobante_Venta_${this.venta!.id}.pdf`);
-        
-        this.generandoPDF = false;
-        this.notificationService.show('PDF generado correctamente', 'success');
-        this.cd.detectChanges(); // Forzamos actualización para reactivar el botón
-      }).catch(err => {
-        console.error(err);
-        this.generandoPDF = false;
-        this.notificationService.show('Error al generar PDF', 'error');
-        this.cd.detectChanges();
-      });
-    } else {
-      console.error('No se encontró el elemento HTML #comprobante-contenido');
-      this.generandoPDF = false;
-    }
-  }
-
-  
   guardarNotas() {
     if (!this.venta) return;
 

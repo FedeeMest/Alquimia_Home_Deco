80f9e6670155814fb20ab46c74d4831847aef35a
Fede <Fede.mestre01@gmail.com>
2026-04-08 18:55:25 -0300

    ad

--- alquimia-front/src/app/components/layout/public-layout/public-layout.ts
+++ alquimia-front/src/app/components/layout/public-layout/public-layout.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './public-layout.html'
 })
 export class PublicLayoutComponent {
   currentYear = new Date().getFullYear();
+
+  // --- LÓGICA DEL MENÚ MÓVIL ---
+  menuAbierto = false;
+
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
+
+  cerrarMenu() {
+    this.menuAbierto = false;
+  }
 }

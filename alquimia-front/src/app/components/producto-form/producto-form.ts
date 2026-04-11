85d815acb01c0548d1267d1091109e337b2ad50d
Fede Mestre <fede.mestre01@gmail.com>
2026-04-11 18:07:34 -0300

    add_cat

--- alquimia-front/src/app/components/producto-form/producto-form.ts
+++ alquimia-front/src/app/components/producto-form/producto-form.ts
@@ -56,7 +56,7 @@ export class ProductoForm implements OnInit {
     'Vasos', 'Copas', 'Tazas', 'Copetineros', 'Tortera', 'Jarras',
     'Compoteras', 'Ensaladeras', 'Jarrones/Floreros', 'Borlas',
     'Accesorio de Mesa', 'Almohadones', 'Mantas', 'Manteles',
-    'Caminos', 'Decoración', 'Difusores / Aromas', 'Ceramica',
+    'Caminos', 'Decoración', 'Difusores / Aromas', 'Ceramica','Textil',
     'Jardín', 'Contenedores', 'Aromatizador', 'Difusores','Jarritos Con Sorbete', 'Otros'
   ];
 

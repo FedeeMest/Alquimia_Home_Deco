dc7f36f6cca74a7bdb1ae3b5776d73c91a019a19
Fede <Fede.mestre01@gmail.com>
2026-04-11 23:49:39 -0300

    fix_not

--- alquimia-front/src/app/app.ts
+++ alquimia-front/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common'; // <--- Importante para *ngIf
 import { RouterOutlet} from '@angular/router'; // <--- Importamos Router
 import { ToastComponent } from '../app/components/ui/toast/toast';
+import { ConfirmDialogComponent } from './components/ui/confirm-dialog/confirm-dialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,  ToastComponent], // <--- Agregamos CommonModule
+  imports: [CommonModule, RouterOutlet,  ToastComponent, ConfirmDialogComponent], // <--- Agregamos CommonModule
   templateUrl: './app.html',
   styleUrl: './app.css' // Asegúrate de que sea styleUrl (singular) o styleUrls (plural) según tu versión, tu código tenía styleUrl
 })

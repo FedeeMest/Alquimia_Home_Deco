5cd634c7e690f20f7349682f9c9775f6b1df5cc0
Fede_Mest <fede.mestre01@gmail.com>
2026-06-11 23:03:52 -0300

    add_mettics_vercel

--- alquimia-front/src/main.ts
+++ alquimia-front/src/main.ts
@@ -3,6 +3,9 @@ import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { inject } from '@vercel/analytics';
+
+inject();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
